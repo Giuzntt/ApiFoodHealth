f782fb06dc2872ed4acfd8593c0cfd6de5f74082
Giulianno Zanetti <giulianno.zanetti@grupomultiplica.com.br>
2022-04-26 15:59:07 -0300

    Primeira Api

---
+++ src/models/Recipes.ts
@@ -0,0 +1,26 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+export interface IRecipe {
+    name: string;
+    description: string;
+    ingredients: string[];
+    instructions: string[];
+    image: string;
+}
+
+export interface IRecipeModel extends IRecipe, Document {}
+
+const RecipeSchema: Schema = new Schema(
+    {
+        name: { type: String, required: true },
+        description: { type: String, required: true },
+        ingredients: { type: [String], required: true },
+        instructions: { type: [String], required: true },
+        image: { type: String, required: false }
+    },
+    {
+        versionKey: false
+    }
+);
+
+export default mongoose.model<IRecipeModel>('Recipe', RecipeSchema);
---
+++ src/server.ts
@@ -0,0 +1,58 @@
+import express from 'express';
+import http from 'http';
+import mongoose from 'mongoose';
+import { config } from './config/config';
+import Logging from './library/Logging';
+
+const router = express();
+
+mongoose
+    .connect(config.mongo.url, { retryWrites: true, w: 'majority' })
+    .then(() => {
+        Logging.info('Connected to MongoDB');
+        StartServer();
+    })
+    .catch((error) => {
+        Logging.error('Error connecting to MongoDB');
+        Logging.error(error);
+    });
+
+const StartServer = () => {
+    router.use((req, res, next) => {
+        // Log the request
+        Logging.info(`Incoming -> Method: [${req.method}]- Url: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
+
+        res.on('finish', () => {
+            // Log the response
+            Logging.info(`Outgoing -> Method: [${req.method}]- Url: [${req.url}] - IP: [${req.socket.remoteAddress}] - Status: [${res.statusCode}]`);
+        });
+        next();
+    });
+    router.use(express.urlencoded({ extended: true }));
+    router.use(express.json());
+    //Rules of our API
+    router.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+        if (req.method === 'OPTIONS') {
+            res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+            return res.status(200).json({});
+        }
+        next();
+    });
+
+    //Routes
+    //Heathcheck
+    router.get('/ping', (req, res) => {
+        res.status(200).json({ message: 'pong' });
+    });
+    router.use((req, res, next) => {
+        const error = new Error('Not found');
+        Logging.error(error);
+        return res.status(404).json({ message: error.message });
+    });
+
+    http.createServer(router).listen(config.server.port, () => {
+        Logging.info(`Server started on port ${config.server.port}`);
+    });
+};
